9ba6a28929e95cee07b286f1443699a2e1a7728d
Emmanuel Ordaz <emmanuelordaz@gmail.com>
2021-02-21 16:53:14 -0600

    Docs agregado

---
+++ docs/precache-manifest.d042aad2a48d58c79f0d6d1b3cfeac60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af40c53104fac3a86f7032ac291f2c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7486582db23da66c082b",
+    "url": "/static/css/main.7e2f9ccb.chunk.css"
+  },
+  {
+    "revision": "95760e856adc85b27085",
+    "url": "/static/js/2.88bb5aee.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.88bb5aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7486582db23da66c082b",
+    "url": "/static/js/main.a4999de4.chunk.js"
+  },
+  {
+    "revision": "bc7f1a1a50efc7af9abe",
+    "url": "/static/js/runtime-main.97c7f426.js"
+  }
+]);
